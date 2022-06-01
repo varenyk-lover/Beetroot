9ca9e0bf51c1cd5d8fa39777a486d6ce660b65bd
varenyk-lover <hanna-horbova@gmail.com>
2022-06-01 19:28:25 +0300

    leeson 15 - init commit

---
+++ lesson-15/scripts/index.js
@@ -0,0 +1,26 @@
+// Тобі потрібно зберігати ім’я та прізвище в змінній, придумай до 4-х імен змінних
+// let name = 'Hanna Horbova';
+// let fullName = 'Hanna Horbova';
+let yourName = 'Hanna Horbova';
+// let firstName = 'Hanna';
+// let lastName = 'Horbova';
+
+// Також напиши до 5 неправильних імен
+// let 1name = 'Hanna Horbova';
+// let ім'я' = 'Hanna Horbova';
+// let !name = 'Hanna Horbova';
+
+
+/*Стилі написання імен змінних:
+camelCase - myName
+snake_case - my_name
+kebab-case - my-name*/
+
+
+// Запитай ім’я користувача та виведи у відповідь “Привіт, *ім’я*”
+let name = prompt('Як вас звати?', Ганна);
+alert('Привіт ' + name)
+console.log(name)
+// Запитай рік народження користувача, порахуй його/її вік і виведи результат. Поточний рік вкажи в коді як константу
+
+// Запитай у користувача довжину сторони квадрату і виведи периметр цього квадрата
